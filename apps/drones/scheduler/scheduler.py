04dbb5ce9ef1542ea9d2c0ebce937b489ebf499d
Danny <mmdanny89@gmail.com>
2023-05-19 23:25:47 -0400

    feat: add register log

--- apps/drones/scheduler/scheduler.py
+++ apps/drones/scheduler/scheduler.py
@@ -1,18 +1,21 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore, register_events
-from django.utils import timezone
-from django_apscheduler.models import DjangoJobExecution
 import sys
+from apps.drones.models import LogBattery, Drone
+
 
 def check_battery_sattus():
-    print('hello world')
+    all_drones = Drone.objects.all()
+    if all_drones:
+        for drone in all_drones:
+            log = LogBattery(log={"drone": drone.serial_number, "batery": drone.get_battery_capacity()})
+            log.save()
 
 
 def start():
     scheduler = BackgroundScheduler()
     scheduler.add_jobstore(DjangoJobStore(), "default")
-    # run this job every 3 minutes
-    scheduler.add_job(check_battery_sattus, 'interval', minutes=3, name='clean_accounts', jobstore='default')
+    scheduler.add_job(check_battery_sattus, 'interval', minutes=1, name='clean_accounts', jobstore='default')
     register_events(scheduler)
     scheduler.start()
     print("Scheduler started...", file=sys.stdout)
